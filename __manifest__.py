2ecd1199bde237449272062a1e62ca8489a00606
unknown <syahrilazis2005@gmail.com>
2023-08-30 09:04:34 +0700

    edit manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,15 @@
 {
-    'name': 'Diskon Tambahan for PO',
+    'name': 'Additional discount for PO',
     'version': '16.0.1.1.0',
-    'summary': 'Diskon Tambahan for PO',
-    'category': 'Productivity',
+    'summary': 'Additional discount for PO',
+    'description': """Insert an additional discount for PO""",
+    'category': 'Inventory/Purchase',
     'author': 'Muhamad Syahril Aziz',
     'license': 'LGPL-3',
     'depends': ['purchase', 'account'],
     'data': ['views/diskon_tambahan.xml',
              'report/diskon_tambahan_report.xml'],
+    'images': ['static/description/icon.svg'],
     'installable': True,
     'auto_install': False
 }
